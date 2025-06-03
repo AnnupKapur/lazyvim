dff6b2f71fad2fa3750501fc9197c71fa0fce624
AnnupKapur <AnnupKapur@gmail.com>
2025-06-03 22:45:30 +0100

    update themes

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -22,11 +22,13 @@ return {
 
   { "kvrohit/substrata.nvim" },
 
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "substrata",
+      colorscheme = "moonfly",
     },
   },
 }
